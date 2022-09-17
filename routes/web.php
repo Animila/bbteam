f2f796d343b96a0786440f52ecfa4a9ebf645797
Animila <hristoforovila5@gmail.com>
2022-09-17 22:42:26 +0900

    Еще один рефакторинг

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,43 @@
 <?php
 
-use App\Models\Chapter;
-use App\Models\Scans;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
-/***
- *
- * ПРОЧЕЕ
- *
- ***/
+/*** ПРОЧЕЕ ***/
 Route::get('/', function () { return view('welcome');})->name('main');
 
-/***
- *
- * РЕГИСТРАЦИЯ И АВТОРИЗАЦИЯ
- *
- ***/
-Route::get('/register', function () {return view('account.register');})->name('register')->middleware('guest');
-Route::post('/register', Account\RegisterController::class)->name('reg')->middleware('guest');
+/*** РЕГИСТРАЦИЯ И АВТОРИЗАЦИЯ ***/
+Route::prefix('/account')->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::get('/register', function () {return view('account.register');})->name('register');
+        Route::post('/register', Account\RegisterController::class)->name('reg');
 
-Route::get('/login', function () {return view('account.login');})->name('login')->middleware('guest');
-Route::post('/auth', Account\LoginController::class)->name('auth')->middleware('guest');
+        Route::get('/login', function () {return view('account.login');})->name('login');
+        Route::post('/auth', Account\LoginController::class)->name('auth');
+    });
 
-Route::get('/logout', function (){Auth::logout();return back();})->name('logout')->middleware('auth');
+    Route::middleware('auth')->group(function () {
+        Route::get('/logout', function (){Auth::logout();return back();})->name('logout');
+    });
 
-/***
- *
- * ПРИВЯЗКА К СОЦИАЛЬНЫМ СЕТЯМ
- *
- ***/
-Route::get('/social-auth/{provider}', function ($provider) {return Socialite::driver($provider)->scopes(['groups'])->redirect();})->name('auth.social');
-Route::get('/social-auth/{provider}/callback', Account\SocialController::class)->name('auth.social.callback');
-Route::get('/social/delete', function (){Account\SocialAccount::where('user_id', auth()->id())->first()->delete();return back();})->name('auth.delete');
+});
 
-/***
- *
- * ПРЕМИУМ
- *
- ***/
-Route::get('/premium/VkDonut', Account\VkDonut::class)->name('premium.DONUT');
-Route::get('/premium/VkDonut/unpin', function (){\auth()->user()->update(['premium'=>0]);return back();})->name('premium.UNDONUT');
+/*** ПРИВЯЗКА К СОЦИАЛЬНЫМ СЕТЯМ ***/
+Route::prefix('/social-auth')->group(function () {
+    Route::get('/{provider}', function ($provider) {return Socialite::driver($provider)->scopes(['groups'])->redirect();})->name('auth.social');
+    Route::get('/{provider}/callback', Account\SocialController::class)->name('auth.social.callback');
+    Route::get('/delete', function (){Account\SocialAccount::where('user_id', auth()->id())->first()->delete();return back();})->name('auth.delete');
+});
 
-/***
- *
- * АДМИН ПАНЕЛЬ
- *
- ***/
-Route::middleware('auth')->prefix('admin')->group(function () {
+/*** ПРЕМИУМ ***/
+Route::prefix('/premium')->middleware('auth')->group(function () {
+    Route::get('/VkDonut', Account\VkDonut::class)->name('premium.DONUT');
+    Route::get('/VkDonut/unpin', function (){\auth()->user()->update(['premium'=>0]);return back();})->name('premium.UNDONUT');
+});
+
+/*** АДМИН ПАНЕЛЬ  ***/
+Route::prefix('admin')->middleware('auth')->group(function () {
 
     Route::prefix('/statistics')->group(function () {
         Route::get('/', Admin\StatisticsController::class)->name('statistics');
@@ -70,12 +59,16 @@ Route::middleware('auth')->prefix('admin')->group(function () {
         Route::get('/{chapter}/edit', Admin\Chapters\EditController::class)->name('chapter.edit');
         Route::patch('/', Admin\Chapters\UpdateController::class)->name('chapter.update');
         Route::delete('/{chapter}',Admin\Chapters\DeleteController::class)->name('chapter.delete');
+
+        Route::prefix('/scan')->group(function () {
+            Route::post('/create', Admin\Chapters\Image\StoreController::class)->name('image.store');
+            Route::patch('/{scan}', Admin\Chapters\Image\UpdateController::class)->name('image.update');
+            Route::delete('/{scan}', Admin\Chapters\Image\DeleteController::class)->name('image.delete');
+        });
     });
 
-    Route::prefix('/scan')->group(function () {
-        Route::post('/create', Admin\Chapters\Image\StoreController::class)->name('image.store');
-        Route::patch('/{scan}', Admin\Chapters\Image\UpdateController::class)->name('image.update');
-        Route::delete('/{scan}', Admin\Chapters\Image\DeleteController::class)->name('image.delete');
+    Route::prefix('/users')->group(function () {
+//        Route::get('/', Admin\Users\IndexController::class)->name('users');
     });
 
 });
